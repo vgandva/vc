216b43a6137887a50894125ebe945a1b92b54f78
vgandva <vgandvr@gmail.com>
2025-01-13 11:17:05 +0530

    Add All Files

---
+++ jb_bas_01/app-files/data.js
@@ -0,0 +1,213 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-1",
+      "name": "living 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.876341056780955,
+        "pitch": 0.19165720738518033,
+        "fov": 1.2424983224529278
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4303446129684394,
+          "pitch": 0.9637940975001946,
+          "rotation": 0,
+          "target": "1-living-2"
+        },
+        {
+          "yaw": 1.6645013404605056,
+          "pitch": 0.3859488793287369,
+          "rotation": 0,
+          "target": "2-dining-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.37849220247167814,
+          "pitch": -0.3045281211524635,
+          "title": "TV UNIT",
+          "text": "SHORT DESCRIPTION ABOUT IT"
+        }
+      ]
+    },
+    {
+      "id": "1-living-2",
+      "name": "living 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8445288611439707,
+          "pitch": 0.5522842988015455,
+          "rotation": 0,
+          "target": "2-dining-1"
+        },
+        {
+          "yaw": -1.6447871285559863,
+          "pitch": 0.7392643887961814,
+          "rotation": 0,
+          "target": "0-living-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dining-1",
+      "name": "dining 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5834874453606362,
+        "pitch": 0.1533019413277099,
+        "fov": 1.2424983224529278
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0148003118667646,
+          "pitch": 0.6468990071883809,
+          "rotation": 0,
+          "target": "1-living-2"
+        },
+        {
+          "yaw": -1.2680390495152132,
+          "pitch": 0.4022536537455004,
+          "rotation": 0,
+          "target": "0-living-1"
+        },
+        {
+          "yaw": -1.757146126724452,
+          "pitch": 0.5558238517985394,
+          "rotation": 0,
+          "target": "3-living3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.807314974183832,
+          "pitch": -0.1111286207551263,
+          "title": "FLUTED PANEL",
+          "text": "<font color=\"#474747\" face=\"Google Sans, Arial, sans-serif\"><span style=\"font-size: 16px; background-color: rgb(255, 255, 255);\">SHORT DESCRIPTION</span></font>"
+        }
+      ]
+    },
+    {
+      "id": "3-living3",
+      "name": "living3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.05155436747654463,
+        "pitch": -0.025533307016932127,
+        "fov": 1.2424983224529278
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5845715150066351,
+          "pitch": 0.711183742468549,
+          "rotation": 0,
+          "target": "0-living-1"
+        },
+        {
+          "yaw": 0.36281860781377695,
+          "pitch": 0.7912852595500404,
+          "rotation": 0,
+          "target": "1-living-2"
+        },
+        {
+          "yaw": 1.7353094649741685,
+          "pitch": 0.4394293020008391,
+          "rotation": 0,
+          "target": "2-dining-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "jayabharath 2bhk twin tower",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
